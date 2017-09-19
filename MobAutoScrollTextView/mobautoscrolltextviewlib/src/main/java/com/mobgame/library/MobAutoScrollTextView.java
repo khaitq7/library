b7bee7dabd00647ec8d257969e1efc2076c92c00
hiendv <hiendv@mobgame.vn>
2017-09-19 10:25:02 +0700

    first commit

---
+++ MobAutoScrollTextView/mobautoscrolltextviewlib/src/main/java/com/mobgame/library/MobAutoScrollTextView.java
@@ -0,0 +1,23 @@
+package com.mobgame.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by HATIBOY on 9/19/2017.
+ */
+
+public class MobAutoScrollTextView extends android.support.v7.widget.AppCompatTextView {
+    public MobAutoScrollTextView(Context context) {
+        super(context);
+    }
+
+    public MobAutoScrollTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MobAutoScrollTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
